912a804896b9f7f140fdfa15e0024920830987a2
Thomas Schweiger <th.schweiger@gmx.net>
2020-03-16 20:53:55 +0100

    more complex query added

--- existApi/nr19_queries.py
+++ existApi/nr19_queries.py
@@ -9,9 +9,15 @@ xquery = f'''
     let $x:= doc("{config.EXIST_DOCUMENT}")
     return $x/Data/result'''
 
+
+xquery = f'''
+    let $x:= doc("{config.EXIST_DOCUMENT}")
+    for $k in $x/Data/result
+    where $k/SPOE < 10 order by $k/DISTRICT_CODE   
+    return $k
+'''
+
 result = client.get(xquery)
-
-
 
 
 
