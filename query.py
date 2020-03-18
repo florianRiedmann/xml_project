3dad34503db47d86ae487c2f82dc6733936bec4f
theRealSpoonman <themrspoonman@gmail.com>
2020-03-18 15:11:15 +0100

    xml schema export

--- query.py
+++ query.py
@@ -2,13 +2,26 @@ from db import connection
 
 # https://docs.microsoft.com/en-us/sql/relational-databases/xml/basic-syntax-of-the-for-xml-clause?view=sql-server-ver15
 
-s = "SELECT * FROM result FOR XML AUTO, TYPE, ELEMENTS, ROOT('Data')"
+# Query for Data
+s = "SELECT * FROM result FOR XML AUTO, TYPE, ELEMENTS, ROOT('Data');"
 
 resultProxy = connection.execute(s)
 rowProxy = resultProxy.fetchone()
 rs = "".join(rowProxy)
-connection.close()
 
 f = open("data/nr19_sprengel.xml", "w")
 f.write(rs)
 f.close()
+
+# Query for Schema
+s = "SELECT * FROM result FOR XML AUTO, ROOT('Data'), XMLSCHEMA;"
+
+resultProxy = connection.execute(s)
+rowProxy = resultProxy.fetchone()
+rs = "".join(rowProxy)
+
+f = open("data/schema.xsd", "w")
+f.write(rs)
+f.close()
+
+connection.close()
