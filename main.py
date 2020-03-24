19e2e04eb4b5de63b35e7f3a5e32f1bca8e5f8ca
Thomas Schweiger <th.schweiger@gmx.net>
2020-03-24 12:17:18 +0100

    queries.py: xqueries refactored; main.py: simplified exist-db calls; exist.py: error handling with printout; config.py: NR19_NAMESPACE added;

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from xml_project.sqlDB.engine import engine
 from xml_project.sqlDB.tables import result
 from xml_project.sqlDB.xml import get_xml_file
 from xml_project.existDB.exist import ExistClient
-from xml_project.existDB.queries import winner, participation
+import xml_project.existDB.queries as query
 from sqlalchemy.exc import SQLAlchemyError
 
+CREATE_DB = False
 
 def main():
     # check if csv exists
@@ -16,57 +17,51 @@ def main():
     file_path = os.path.join(project_path, "data", file_name)
 
     # prepare data for insert statement
-    if os.path.isfile(file_path):
-        data = prepare_data(file_path)
-    else:
-        print(f"{file_name} does not exist!")
+    if CREATE_DB:
+        if os.path.isfile(file_path):
+            data = prepare_data(file_path)
+        else:
+            print(f"{file_name} does not exist!")
 
-    # create table
-    print("CREATE TABLE")
-    try:
-        result.create(engine)
-    except SQLAlchemyError as e:
-        print("Error: There is already an object named 'result'!")
+        # create table
+        print("CREATE TABLE")
+        try:
+            result.create(engine)
+        except SQLAlchemyError as e:
+            print("Error: There is already an object named 'result'!")
 
-    print("INSERT INTO")
-    # insert statement
-    try:
-        with engine.connect() as connection:
-            connection.execute(result.insert(), data)
-    except SQLAlchemyError:
-        print("Error: Cannot insert duplicate key in object!")
+        print("INSERT INTO")
+        # insert statement
+        try:
+            with engine.connect() as connection:
+                connection.execute(result.insert(), data)
+        except SQLAlchemyError:
+            print("Error: Cannot insert duplicate key in object!")
 
-    # query xml from database and export
-    xml_file_name = XML_FILE_NAME
-    get_xml_file(os.path.join(project_path, "data", xml_file_name))
-
-
-def query_winner_nr19():
-    # query from existDB
-    try:
-        client = ExistClient()
-        rs = client.get(winner)
-        return rs
-    except Exception as e:
-        print("SQL Database Error:")
-        print(e.args)
+        # query xml from database and export
+        xml_file_name = XML_FILE_NAME
+        get_xml_file(os.path.join(project_path, "data", xml_file_name))
 
 
-def query_part_nr19():
-    # query from existDB
-    try:
-        client = ExistClient()
-        rs = client.get(participation)
-        return rs
-    except Exception as e:
-        print("SQL Database Error:")
-        print(e.args)
 
 
 if __name__ == "__main__":
     main()
-    print("\nDistrict Winner: ")
-    [print(i) for i in query_winner_nr19()]
+    client = ExistClient()
+
+    print("\nAbsolute Result for OEVP in District 5: ")
+    [print(i) for i in client.get(query.absolut_votes_by_party_and_district('OEVP', 5))]
+
+    print("\nRelative Result for GRUE in District 8: ")
+    [print(i) for i in client.get(query.relative_result_by_party_and_district('GRUE', 8))]
+
+
+    print("\nDistrict Relative Results ordered by Party: ")
+    [print(i) for i in client.get(query.relative_results_ordered_by_party)]
+    print("\nDistrict Relative Results ordered by Result: ")
+    [print(i) for i in client.get(query.relative_results_ordered_by_result)]
     print("\nDistrict Voter Participation: ")
-    [print(i) for i in query_part_nr19()]
+    [print(i) for i in client.get(query.participation)]
+    print("\nDistrict Winner: ")
+    [print(i) for i in client.get(query.winner)]
 
